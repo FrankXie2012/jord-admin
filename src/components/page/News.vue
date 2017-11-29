f8321317d1942735b3ef4e93d67fb55032af95fd
FrankXie2012 <fr6nkxie@gmail.com>
2017-11-30 00:56:06 +0800

    'mammoth'

--- src/components/page/News.vue
+++ src/components/page/News.vue
@@ -1,13 +1,27 @@
 <template>
-  <pdf src="./static/news.pdf"></pdf>
+<el-button type="primary" @click="getData">读取doc文件</el-button>
 </template>
 
 <script>
-import pdf from 'vue-pdf';
+import mammoth from 'mammoth/mammoth.browser.js'
 
 export default {
-  components: {
-    pdf
-  }
+    components: {
+        mammoth
+    },
+    methods: {
+        getData() {
+            console.info(mammoth);
+            mammoth.convertToHtml({
+                    path: "./static/demo.doc"
+                })
+                .then(function(result) {
+                    console.info(result)
+                    var html = result.value; // The generated HTML
+                    var messages = result.messages; // Any messages, such as warnings during conversion
+                })
+                .done();
+        }
+    }
 }
 </script>
