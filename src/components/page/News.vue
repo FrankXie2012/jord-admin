65793cabae1f4163897e14c466207ed7e707f759
FrankXie2012 <fr6nkxie@gmail.com>
2017-12-02 00:06:43 +0800

    Upload news.

--- src/components/page/News.vue
+++ src/components/page/News.vue
@@ -1,5 +1,12 @@
 <template>
-<el-button type="primary" @click="getData">读取doc文件</el-button>
+<div>
+    <el-upload class="upload-demo" drag action :on-change="getData" :file-list="fileList" :accept="docx">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传docx文件，且不超过500kb</div>
+    </el-upload>
+    <div v-html="article"></div>
+</div>
 </template>
 
 <script>
@@ -9,18 +16,27 @@ export default {
     components: {
         mammoth
     },
+    data: function() {
+        return {
+            fileList: [],
+            article: ''
+        }
+    },
     methods: {
-        getData() {
-            console.info(mammoth);
-            mammoth.convertToHtml({
-                    path: "./static/demo.doc"
-                })
-                .then(function(result) {
-                    console.info(result)
-                    var html = result.value; // The generated HTML
-                    var messages = result.messages; // Any messages, such as warnings during conversion
-                })
-                .done();
+        getData(file, fileList) {
+            var self = this;
+            var reader = new FileReader();
+            reader.onload = function(loadEvent) {
+                var arrayBuffer = loadEvent.target.result;
+                mammoth.convertToHtml({
+                        arrayBuffer: arrayBuffer
+                    })
+                    .then(function(result) {
+                        self.article = result.value;
+                    })
+                    .done();
+            };
+            reader.readAsArrayBuffer(file.raw);
         }
     }
 }
