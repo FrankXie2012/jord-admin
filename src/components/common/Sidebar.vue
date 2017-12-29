b849e73e7d69655ca4bc9ad2a4ae1f07abd4b115
FrankXie2012 <fr6nkxie@gmail.com>
2017-12-30 00:25:16 +0800

    Production config.

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -50,13 +50,13 @@ export default {
                     icon: 'el-icon-star-off',
                     index: 'users',
                     title: '用户管理',
-                    class: (_role !== 'admin' ? '' : 'hidden')
+                    class: (_role !== 'admin' ? 'hidden' : '')
                 },
                 {
                     icon: 'el-icon-setting',
                     index: 'person',
                     title: '账号信息',
-                    class: (_role !== 'admin' ? '' : 'hidden')
+                    class: (_role !== 'admin' ? 'hidden' : '')
                 }
             ]
         }
